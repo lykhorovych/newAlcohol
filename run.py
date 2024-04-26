5a283e884b424ede4386b0034817fe3707590819
olykhorovych <olehlykhorovych@gmail.com>
2024-04-27 01:41:08 +0300

    some changes in workflow file and run.py

--- run.py
+++ run.py
@@ -10,10 +10,11 @@ from dotenv import load_dotenv
 import dj_database_url
 
 
-load_dotenv()
-
-
 BASE_DIR = Path(__file__).resolve().parent
+
+
+load_dotenv(dotenv_path=BASE_DIR / '.env.dev')
+
 print(BASE_DIR)
 INSTALLED_APPS = ['visky']
 DATABASES = {
