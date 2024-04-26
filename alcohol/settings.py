f66564a76819555eefff48cba75608ebc0a8341a
olykhorovych <olehlykhorovych@gmail.com>
2024-04-26 11:16:41 +0300

    configure setiings.py

--- alcohol/settings.py
+++ alcohol/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.getenv('DEBUG', default=True))
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(" ")
 
 
 # Application definition
