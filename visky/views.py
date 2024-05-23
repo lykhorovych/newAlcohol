ee934a2478b1e9992a9a518bb0771b09ac78a3be
olykhorovych <olehlykhorovych@gmail.com>
2024-05-23 22:37:38 +0300

    add plot for change prices of alcohols

--- visky/views.py
+++ visky/views.py
@@ -1,3 +1,8 @@
+import io
+import urllib, base64
+from typing import Any
+import urllib.parse
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.http import JsonResponse
@@ -5,6 +10,7 @@ from django.views.generic import ListView, DetailView
 from .models import Alcohol
 from .forms import AlcoForm
 from django.core.paginator import Paginator
+import plotly.graph_objects as go 
 # Create your views here.
 
 
@@ -29,3 +35,19 @@ class DetailAlco(DetailView):
     template_name = "visky/detail.html"
     context_object_name = "alcohol"
 
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        alcohol = Alcohol.objects.get(pk=pk)
+    
+        dates = [k for d in alcohol.prices for k in d]
+        prices = [k for d in alcohol.prices for k in d.values()]
+
+        fig = go.Figure([go.Scatter(x=dates, y=prices)])
+        fig = fig.to_image(format='png')
+
+        context['alcohol'] = alcohol
+        context['fig'] = base64.b64encode(fig).decode()
+
+        return context
+    
