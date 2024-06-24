ab0e5e9c89f90b149b1c4a0b378d92c3c520f185
olykhorovych <olehlykhorovych@gmail.com>
2024-06-24 14:58:18 +0300

    add kaisedo package to requirements and change path in urls.py

--- alcohol/urls.py
+++ alcohol/urls.py
@@ -23,7 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('visky.urls')),
-    path('economy/', include('products.urls')),
+    path('economy', include('products.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
