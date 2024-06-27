a6c2db70c27e5b06c2b742842a4b2aad8e6cb1aa
olykhorovych <olehlykhorovych@gmail.com>
2024-06-27 12:45:42 +0300

    download photos to static by photos url

--- scripts/download_photos.py
+++ scripts/download_photos.py
@@ -1,10 +1,18 @@
 import subprocess
 from products.models import Product
-
+from visky.models import Alcohol
+from alcohol.settings import BASE_DIR
 
 def run():
     for product in Product.objects.all():
-        print(["wget", "-b", f"{product.image}", "-P", r"./static/images"])
-        subprocess.run(["wget", "-b", f"{product.image}", "-P", r"./static/images"])
+        subprocess.run(["wget",
+                        "-b", f"{product.image}", 
+                        "-P", BASE_DIR / "static/images/economy"])
 
+    #for alcohol in Alcohol.objects.values_list('image'):
+    #    link, *_ = alcohol
+    #    subprocess.run(["wget",
+    #                     "-b", f"{link}",
+    #                     "-P", BASE_DIR / "static/images/visky"])
 
+    return 1
