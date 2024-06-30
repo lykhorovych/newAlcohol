f89231b1874e5aa24a7c077feb706ac035de16a9
olykhorovych <olehlykhorovych@gmail.com>
2024-06-30 17:03:32 +0300

    change folder to downloading photos

--- scripts/download_photos.py
+++ scripts/download_photos.py
@@ -3,16 +3,15 @@ from products.models import Product
 from visky.models import Alcohol
 from django.conf import settings
 
-
 def run():
     for link, *_ in Product.objects.values_list('image'):
         subprocess.run(["wget",
                         "-b", f"{link}",
-                        "-P", settings.STATICFILES_DIRS[0] + "/images/economy"])
+                        "-P", settings.STATICFILES_DIRS[0]])
 
     for link, *_ in Alcohol.objects.values_list('image'):
         subprocess.run(["wget",
                         "-b", f"{link}",
-                        "-P", settings.STATICFILES_DIRS[0] + "/images/alcohol"])
+                        "-P", settings.STATICFILES_DIRS[0]])
 
     return 1
