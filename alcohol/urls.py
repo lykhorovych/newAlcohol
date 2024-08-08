549b1207808cc26965ce539700be9bf18a44d8c3
olykhorovych <olehlykhorovych@gmail.com>
2024-08-08 12:18:57 +0300

    add debug-toolbar in DEBUG mod

--- alcohol/urls.py
+++ alcohol/urls.py
@@ -18,13 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('visky.urls')),
-    path('economy', include('products.urls')),
-] + debug_toolbar_urls()
+    path('economy', include('products.urls')),]
+
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+    urlpatterns += debug_toolbar_urls()
+
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
